eb5a7bc33d757d467094b5e7493b40af6c5d650e
Miriam Zimmerman <mutexlox@signal.org>
2024-10-10 11:45:25 -0400

    Disable broken clippy nightly check.
    See https://github.com/rust-lang/rust-clippy/issues/13531

--- src/backend/context.rs
+++ src/backend/context.rs
@@ -270,6 +270,8 @@ impl ContextOps for PulseContext {
     }
 
     fn backend_id(&mut self) -> &'static CStr {
+        // https://github.com/rust-lang/rust-clippy/issues/13531
+        #[allow(clippy::manual_c_str_literals)]
         unsafe { CStr::from_ptr(b"pulse-rust\0".as_ptr() as *const _) }
     }
 
